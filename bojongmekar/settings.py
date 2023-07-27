4efc2eb7ba943e5c33388ce2927c9447e4bb99d2
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-07-27 12:52:48 +0700

    fix error

--- bojongmekar/settings.py
+++ bojongmekar/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l)=-xiww#t%=8sr^eeprd-s5h7liqlq*xfw%*teshb77dr-$yl'
+SECRET_KEY = '{DJANGO_SECRET_KEY}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bojongmekar-web.domcloud.io', 'bojongtesting-app-app-04a7d7f651edd0fc4-duefjrmtca-uc.a.run.app', '.localhost', '127.0.0.1']
 
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from main.models import Feedback
 from django.core import serializers
 
 from artikel.models import Article
+from berita.models import News
 
 # Create your views here.
 def index(request):
@@ -17,8 +18,11 @@ def index(request):
             return HttpResponseRedirect(reverse('main:index'))
     
     articles = Article.objects.all()
+    news = News.objects.all()
+
     context = {
         'article_list': articles,
+        'news_list': news,
         'form': FeedbackForm()
     }
     return render(request, "home.html", context)
@@ -26,6 +30,7 @@ def index(request):
 def show_json(request):
     data = Feedback.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
 
 def add_feedback(request):  
     # create object of form
