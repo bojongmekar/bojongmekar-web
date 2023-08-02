f78a5dd6d1e5a8387f3df6bf0f3d15144b1a0f7f
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-08-02 19:49:08 +0700

    add finishing touches

--- berita/models.py
+++ berita/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    photo_link = models.URLField(max_length=200, default='')
+    photo_link = models.URLField(max_length=200, default='https://www.airspiralo.com/media/products/notfound.png')
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
